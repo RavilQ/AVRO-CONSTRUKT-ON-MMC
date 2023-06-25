542c2d48779514be863cc1538ccbb2f87b0ef37a
Ravil <ravil.quliyev.98@bk.ru>
2023-06-25 20:45:44 +0400

    Add project files.

---
+++ AVRO CONSTRUKTİON MMC/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace AVRO_CONSTRUKTİON_MMC.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
