b393c20d61da1c0e850c51ab18030608869c4f77
TAHiRLI <tahrli.tahirr@gmail.com>
2023-06-29 21:22:56 +0400

    team page added

---
+++ AVRO CONSTRUKTİON MMC/Controllers/EmployeeController.cs
@@ -0,0 +1,24 @@
+﻿using AVRO_CONSTRUKTİON_MMC.DAL;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AVRO_CONSTRUKTİON_MMC.Controllers
+{
+    public class EmployeeController:Controller
+    {
+        private readonly AvroConstructionDbContext _context;
+
+        public EmployeeController(AvroConstructionDbContext context)
+        {
+            _context = context;
+        }
+
+        //=========================
+        // Our Team page
+        //=========================
+
+        public IActionResult OurTeam()
+        {
+            return View();
+        }
+    }
+}
