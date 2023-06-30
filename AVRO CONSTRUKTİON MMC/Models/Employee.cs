3afce2a457b47554c6e176f65f3141c5d340d9ca
Ravil <ravil.quliyev.98@bk.ru>
2023-06-30 23:46:14 +0400

    Add MaxLength and BtnLink

--- AVRO CONSTRUKTİON MMC/Models/Employee.cs
+++ AVRO CONSTRUKTİON MMC/Models/Employee.cs
@@ -1,9 +1,13 @@
-﻿namespace AVRO_CONSTRUKTİON_MMC.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AVRO_CONSTRUKTİON_MMC.Models
 {
     public class Employee:BaseEntity
     {
+        [MaxLength(30)]
         public string Fullname { get; set; }
         public int JobId { get; set; }
+        [MaxLength(100)]
         public string Image { get; set; }
 
         public Job Job { get; set; }
--- AVRO CONSTRUKTİON MMC/Models/Service.cs
+++ AVRO CONSTRUKTİON MMC/Models/Service.cs
@@ -1,9 +1,14 @@
-﻿namespace AVRO_CONSTRUKTİON_MMC.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AVRO_CONSTRUKTİON_MMC.Models
 {
     public class Service:BaseEntity
     {
+        [MaxLength(50)]
         public string Title { get; set; }
+        [MaxLength(500)]
         public string Text { get; set; }
+        [MaxLength(100)]
         public string Icon { get; set; }
     }
 }
