dd050fc5310fc12cfff5fd80806409b374ba749b
TAHiRLI <tahrli.tahirr@gmail.com>
2023-07-07 21:09:47 +0400

    Email Service Added

---
+++ AVRO CONSTRUKTİON MMC/Helpers/Interfaces/IEmailSender.cs
@@ -0,0 +1,10 @@
+﻿using AVRO_CONSTRUKTİON_MMC.Utilities;
+
+namespace AVRO_CONSTRUKTİON_MMC.Helpers.Interfaces
+{
+	public interface IEmailSender
+	{
+        void SendEmail(Message message);
+
+    }
+}
