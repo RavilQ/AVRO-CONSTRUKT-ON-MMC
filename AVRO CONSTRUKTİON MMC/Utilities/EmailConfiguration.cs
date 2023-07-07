879c4c10014e099f0ec0a28370a02cd64fe314c6
TAHiRLI <tahrli.tahirr@gmail.com>
2023-07-07 19:27:55 +0400

    email configuration added

---
+++ AVRO CONSTRUKTİON MMC/Utilities/EmailConfiguration.cs
@@ -0,0 +1,14 @@
+﻿namespace AVRO_CONSTRUKTİON_MMC.Utilities
+{
+	public class EmailConfiguration
+	{
+        public class EmailConfiguration
+        {
+            public string From { get; set; }
+            public string SmtpServer { get; set; }
+            public int Port { get; set; }
+            public string UserName { get; set; }
+            public string Password { get; set; }
+        }
+    }
+}
