04fed89bd580968e9297d12d82069d180273551b
TAHiRLI <tahrli.tahirr@gmail.com>
2023-07-12 16:44:48 +0400

    Project Model and views added

---
+++ AVRO CONSTRUKTİON MMC/Controllers/ProjectController.cs
@@ -0,0 +1,34 @@
+﻿using AVRO_CONSTRUKTİON_MMC.DAL;
+using AVRO_CONSTRUKTİON_MMC.ViewModel;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AVRO_CONSTRUKTİON_MMC.Controllers
+{
+    public class ProjectController : Controller
+    {
+        private readonly AvroConstructionDbContext _context;
+
+        public ProjectController(AvroConstructionDbContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            var model = new ProjectsViewModel()
+            {
+                Projects = _context.Projects.ToList()
+        };
+
+            return View(model);
+        }
+        public IActionResult Details(int id)
+        {
+            var project = _context.Projects.Find(id);
+            if (project == null)
+                return NotFound();
+            
+
+            return View(project);
+        }
+    }
+}
---
+++ AVRO CONSTRUKTİON MMC/Migrations/20230711090729_ProjectsTableCreated.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AVRO_CONSTRUKTİON_MMC.Migrations
+{
+    public partial class ProjectsTableCreated : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Projects",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(max)", maxLength: 5000, nullable: false),
+                    Image = table.Column<string>(type: "nvarchar(200)", maxLength: 200, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Projects", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Projects");
+        }
+    }
+}
