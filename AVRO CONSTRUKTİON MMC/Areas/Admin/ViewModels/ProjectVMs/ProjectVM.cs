8fd156fcc4b57de13098a52e093432e4be92557e
TAHiRLI <tahrli.tahirr@gmail.com>
2023-07-13 23:59:36 +0400

    done

---
+++ AVRO CONSTRUKTİON MMC/Areas/Admin/ViewModels/ProjectVMs/ProjectVM.cs
@@ -0,0 +1,9 @@
+﻿using AVRO_CONSTRUKTİON_MMC.Models;
+
+namespace AVRO_CONSTRUKTİON_MMC.Areas.Admin.ViewModels.ProjectVMs
+{
+    public class ProjectVM
+    {
+        public ICollection<Project> Projects { get; set; }
+    }
+}
