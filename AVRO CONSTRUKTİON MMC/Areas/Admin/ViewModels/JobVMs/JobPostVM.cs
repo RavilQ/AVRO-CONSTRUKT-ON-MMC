2212f40efc54df2811d67ffcd7c0d38bb5739346
TAHiRLI <tahrli.tahirr@gmail.com>
2023-07-15 21:49:33 +0400

    employee crud done closes #43

--- AVRO CONSTRUKTİON MMC/Areas/Admin/ViewModels/JobVMs/JobPostVM.cs
+++ AVRO CONSTRUKTİON MMC/Areas/Admin/ViewModels/JobVMs/JobPostVM.cs
@@ -5,7 +5,7 @@ namespace AVRO_CONSTRUKTİON_MMC.Areas.Admin.ViewModels.JobVMs
     public class JobPostVM
     {
         public int? Id { get; set; }
-        [MaxLength(20,ErrorMessage = "Maksimum uzunluq 20 olmalıdır")]
+        [MaxLength(20,ErrorMessage = "20 hərfdən artıq olmamalıdır")]
         public string Name { get; set; }
     }
 }
