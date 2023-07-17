41436e681a3edd09fc28e879ea040a95b6a68ce7
TAHiRLI <tahrli.tahirr@gmail.com>
2023-07-17 19:17:22 +0400

    home featured projects done closes #52

--- AVRO CONSTRUKTİON MMC/ViewModel/HomeViewModel.cs
+++ AVRO CONSTRUKTİON MMC/ViewModel/HomeViewModel.cs
@@ -4,10 +4,12 @@ namespace AVRO_CONSTRUKTİON_MMC.ViewModel
 {
     public class HomeViewModel
     {
-       public ICollection<Slider> Sliders { get; set; } 
-       public ICollection<Employee> Employees { get; set; }
-       public ICollection<Testimonials> Testimonials { get; set; }
-       public ICollection<Service> Services { get; set; } 
+        public ICollection<Slider> Sliders { get; set; }
+        public ICollection<Employee> Employees { get; set; }
+        public ICollection<Testimonials> Testimonials { get; set; }
+        public ICollection<Service> Services { get; set; }
+        public ICollection<Project> Projects { get; set; }
         public IDictionary<string, string?> Settings { get; set; }
+
     }
 }
